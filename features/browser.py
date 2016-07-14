671a164e0110989477809e519bf16e8052d5c3a2
dc-artem <a.fedorchuk@delivery-club.ru>
2016-07-14 16:42:22 +0300

    remote

--- features/browser.py
+++ features/browser.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 
 
 class Browser(object):
-    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+    driver = webdriver.Remote(
+    desired_capabilities=webdriver.DesiredCapabilities.FIREFOX,
+    command_executor='http://192.168.1.123:4444/wd/hub')
+    #driver = webdriver.Chrome('/usr/local/bin/chromedriver')
     driver.implicitly_wait(5)
     #driver.set_page_load_timeout(30)
     driver.maximize_window()
